901430620235e4261e6fd7f95e14459ada30d9ad
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-04-29 17:19:44 -0300

    Create 14.c
    Xd

---
+++ entrada_salida/14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    FILE *in = fopen(argv[1], "r");
+    FILE *out = fopen(argv[2], "w");
+    int c;
+    while((c = fgetc(in)) != EOF) {
+      fputc(c, out);
+    }
+  fclose(in);
+  fclose(out);
+  return 0;
+}
