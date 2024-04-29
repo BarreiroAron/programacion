f3c59708227266e4f5c2ac14c569e4e2c78edceb
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-04-29 17:07:38 -0300

    Create 2.c
    Xd

---
+++ entrada_salida/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int n = atoi(argv[1]);
+  int sum = 0;
+  for(int i = 0; i < n; i++) {
+    int num;
+    scanf("%d", &num);
+    sum += num;
+  }
+  printf("%d\n", sum);
+  return 0;
